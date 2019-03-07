0d4d21f90893e65a4007c4b310ef9f8fea14921f
derpoliuk <derpoliuk@gmail.com>
2019-03-07 16:40:54 +0200

    Add ClassProtocolExtension playground

---
+++ ClassProtocolExtension.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+protocol Foo {}
+
+class Bar: Foo {
+
+    func someFunction() {
+        print("Bar class")
+    }
+
+}
+
+extension Foo {
+
+    func someFunction() {
+        print("Foo class")
+    }
+
+}
+
+let bar = Bar()
+bar.someFunction()
+
+let foo: Foo = bar
+foo.someFunction()
